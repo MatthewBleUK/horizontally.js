2f686040f75945492b4ec6607eb8c0a1982db776
MatthewBleUK <matthew@bleathman.com>
2023-07-02 21:03:38 +0300

    Update to rollup config file and package keywords + description

--- rollup.config.js
+++ rollup.config.js
@@ -2,30 +2,25 @@ import css from "rollup-plugin-import-css";
 import { terser } from "rollup-plugin-terser";
 
 export default {
-	input: 'src/js/horizontally.js',
-	output: [
-		{
-			name: 'horizontally',
-			file: 'dist/horizontally.js',
-			format: 'umd',
-		}, 
-		{
-			name: 'horizontally',
-			file: 'dist/horizontally.min.js',
-			plugins: [
-				terser({
-					compress: {
-						passes: 2,
-						drop_console: true,
-					}
-				})
-			],
-			format: 'umd',
-		}       
-	],
-	plugins: [ 
-		css({
-			minify: true
-		})
-	]
+    input: "src/js/horizontally.js",
+    output: [
+        {
+            // Standard file output
+            name: "horizontally",
+            file: "dist/horizontally.js",
+        },
+        {
+            // Min file output
+            name: "horizontally",
+            file: "dist/horizontally.min.js",
+            format: "umd",
+            plugins: [terser()],
+        },
+    ],
+    plugins: [
+        css({
+            output: "horizontally.css",
+            minify: false,
+        }),
+    ],
 };
